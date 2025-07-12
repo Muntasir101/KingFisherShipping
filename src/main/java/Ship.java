0476e80f3e13a8383670b0c60120005dbc72350e
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2025-07-12 23:16:58 +0600

    project create and Ship class start

---
+++ src/main/java/Ship.java
@@ -0,0 +1,7 @@
+public class Ship {
+    private String name;
+    private double capacityMT;
+    private double receivedFrom;
+    private double currentLoadMT;
+    private String destination;
+}
