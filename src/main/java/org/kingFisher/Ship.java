fb723caeefd362feb79c4176e2fa28d4a3c96a55
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2025-07-18 11:43:57 +0600

    validations

--- src/main/java/org/kingFisher/Ship.java
+++ src/main/java/org/kingFisher/Ship.java
@@ -8,7 +8,7 @@ public class Ship {
     private String destination;
 
     private static int totalShips = 0;
-    private static final String Safety_standard = "IMO 2020";
+    private static final String SAFETY_STANDARD = "IMO 2020";
 
     // constructor
     public Ship(String name, double capacityMT)
@@ -21,15 +21,31 @@ public class Ship {
 
     // instance methods
     public void loadOil(double tons){
+        //Validation 1: Load oil should not be negative
         if(tons <=0){
             System.out.println("Invalid Load.Oil Load should not Zero or Negative.");
+            return;
+        }
+        // Validation 2: Load oil should not exceed capacity.
+        if(currentLoadMT + tons > capacityMT ){
+            System.out.println("Load Exceeds ship's Capacity.");
             return;
         }
         currentLoadMT += tons;
         System.out.println(name + " Loaded "+ tons + " Mt of oil.");
     }
-    public void unload(){
+    public void unload(double tons){
+        if(tons <=0){
+            System.out.println("Invalid Upload.Un Load should not Zero or Negative.");
+            return;
+        }
+        // validation: Unload should not exceed Current Load
+        if(tons > currentLoadMT){
+            System.out.println("Invalid.Unload should not exceed Current Load!!");
+            return;
+        }
         System.out.println(name + " Unloaded "+ currentLoadMT + " Mt of oil.");
+        currentLoadMT -= tons;
     }
     public boolean isOverLoaded(){
         return currentLoadMT > capacityMT;
@@ -45,7 +61,7 @@ public class Ship {
 
     // static methods
     public static String getSafetyStandard(){
-        return Safety_standard;
+        return SAFETY_STANDARD;
     }
     public static int getTotalShips(){
         return totalShips;
@@ -55,7 +71,7 @@ public class Ship {
         Ship ship1 = new Ship("OceanTitan",1000);
         System.out.println("New org.kingFisher.Ship created. Total Ships: "+ getTotalShips());
         System.out.println(ship1.name +" Safety Standard: "+getSafetyStandard());
-        ship1.loadOil(0);
+        ship1.loadOil(950);
 
         if(ship1.isOverLoaded()){
             System.out.println("Warning "+ship1.name + " is overloaded!!");
@@ -64,20 +80,20 @@ public class Ship {
         ship1.setDestination("CTG");
 
         System.out.println(ship1.name+ " Current Load "+ ship1.getCurrentLoad() + " MT");
-        ship1.unload();
+        ship1.unload(980);
 
 
         Ship ship2 = new Ship("KingFisher",10000);
         System.out.println("New org.kingFisher.Ship created. Total Ships: "+ getTotalShips());
         System.out.println(ship2.name +" Safety Standard: "+getSafetyStandard());
-        ship2.loadOil(-100);
+        ship2.loadOil(10000);
         if(ship2.isOverLoaded()){
             System.out.println("Warning "+ship2.name + " is overloaded!!");
         }
         ship2.setDestination("CTG");
 
         System.out.println(ship2.name + " Current Load "+ ship2.getCurrentLoad() + " MT");
-        ship2.unload();
+        ship2.unload(20000);
 
     }
 
